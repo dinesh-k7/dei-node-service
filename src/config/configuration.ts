443a8e47feebdcfb026b92d9a971e48868f03275
unknown <dineshiie@gmail.com>
2021-02-02 14:20:42 +0530

    Revert email config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -7,13 +7,13 @@ export default (): any => ({
     minutes: 10,
   },
   EMAIL_CONFIGURATION: {
-    EMAIL_HOST: process.env.EMAIL_HOST || 'smtp.gmail.com',
+    EMAIL_HOST: process.env.EMAIL_HOST || 'smtpout.secureserver.net',
     EMAIL_PORT: process.env.EMAIL_PORT || 587,
     SECURE_PORT: process.env.SECURE_PORT || 465,
-    SECURE: process.env.SECURE || false,
-    EMAIL_ID: process.env.EMAIL_ID || 'dineshiie@gmail.com',
-    EMAIL_PASS: process.env.EMAIL_PASS || 'asalmangatha2011',
-    EMAIL_FROM: process.env.EMAIL_FROM || 'dineshiie@gmail.com',
+    SECURE: process.env.SECURE || true,
+    EMAIL_ID: process.env.EMAIL_ID || 'DoNotReply@xiiiusa.com',
+    EMAIL_PASS: process.env.EMAIL_PASS || 'LJHyy3JmN*ceX_x_pLR',
+    EMAIL_FROM: process.env.EMAIL_FROM || 'DoNotReply@xiiiusa.com',
     CONFIRMATION_EMAIL_SUBJECT:
       process.env.CONFIRMATION_EMAIL_SUBJECT ||
       'Digital Enterprise Initiative™ confirmation',
